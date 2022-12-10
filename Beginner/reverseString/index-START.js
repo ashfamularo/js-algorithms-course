c9bc78929ce8649592afb30a67a5262bf556e9b1
Ashley Famularo <ash0fam@gmail.com>
2022-12-09 22:13:54 -0500

    First commit

---
+++ Beginner/reverseString/index-START.js
@@ -0,0 +1,24 @@
+/* CHALLENGE
+Given a string of text, write an algorithm that returns the text received in a reversed format. 
+E.g reverseString('algorithms') // should return 'smhtirogla'
+*/
+
+// chaining in-built methods
+//! function reverseString(text) {
+//!     return text.split('').reverse().join('')
+//! }
+
+
+// using a for-loop
+
+// recursion method
+
+// using .reduce()
+
+function reverseString(text) {
+    // Code goes here
+}
+
+
+
+module.exports = reverseString
